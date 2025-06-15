205290876d157565beb93b7c762a2e7070f1d507
고은서 <gogi0827@gmail.com>
2025-06-16 00:52:45 +0900

    두번째맵 주소 지정 오류

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import Phaser from 'phaser';
 import GameScene from './phaser/GameScene';
-import SecondMapScene from './phaser/SecondMapScene';
+import SecondMapScene from './phaser/secondMapScene';
 import ThirdMapScene from './phaser/ThirdMapScene';
 import FishingResultScene from './phaser/FishingResultScene';
 
